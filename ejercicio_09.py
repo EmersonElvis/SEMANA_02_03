a23d3e15ef771ebef18d9fcda26252c42ebc5c08
Emerson Elvis <121317662+EmersonElvis@users.noreply.github.com>
2023-01-06 10:46:21 -0500

    Update ejercicio_09.py

--- ejercicio_09.py
+++ ejercicio_09.py
@@ -3,12 +3,15 @@ dos números a + b. Considera que la suma
 a+b = a + 1 + 1 + ...+ 1 (b veces)'''
 
 def suma_recursiva(a,b):
-    if a==0:
-        suma=b
+    if a==0 and b!=0:
+        suma=b+suma_recursiva(a,b-1)
+    elif b==0 and a!= 0:
+        suma=a
+    elif a==0 and b==0:
+        suma = 0
     else:
-        if b==0:
-            suma=a
-        else:
-            suma = a+suma_recursiva(a,b-1)
-    return suma 
-print(suma_recursiva(5,7))
+        suma = a+b+suma_recursiva(a,b-1)
+        if b==1:
+            return suma-a
+    return suma
+print(suma_recursiva(45,79))
